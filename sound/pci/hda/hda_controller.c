FlyGoat/linux-gs-tweak

Loongson: Add LS2H/LS7A HD-Audio support
Fixed By: Jiaxun Yang <jiaxun.yang@flygoat.com>
Signed-off-by: Huacai Chen <chenhc@lemote.com>

sound/pci/hda/hda_controller.c
@@ -309,6 +309,23 @@ unsigned int azx_get_position(struct azx *chip,
 	int stream = substream->stream;
 	int delay = 0;
 
+	if (chip->driver_caps & AZX_DCAPS_LS2X_WORKAROUND) {
+		pos = chip->get_position[stream](chip, azx_dev);
+
+		if (pos >= azx_dev->fix_prvpos) {
+			pos = pos - azx_dev->fix_prvpos;
+			pos %= azx_dev->core.bufsize;
+		} else {
+			if (azx_dev->fix_prvpos > azx_dev->core.bufsize)
+				pos = (0x100000000ULL + pos-azx_dev->fix_prvpos)
+					% azx_dev->core.bufsize;
+			else
+				pos = pos + azx_dev->core.bufsize - azx_dev->fix_prvpos;
+		}
+
+		return pos;
+	}
+
 	if (chip->get_position[stream])
 		pos = chip->get_position[stream](chip, azx_dev);
 	else /* use the position buffer as default */
@@ -803,8 +820,11 @@ static int azx_rirb_get_response(struct hdac_bus *bus, unsigned int addr,
 
 	for (loopcounter = 0;; loopcounter++) {
 		spin_lock_irq(&bus->reg_lock);
-		if (chip->polling_mode || do_poll)
+		if (chip->polling_mode || do_poll) {
+			if (chip->driver_caps & AZX_DCAPS_LS2X_WORKAROUND)
+				bus->rirb.cmds[addr] %= AZX_MAX_RIRB_ENTRIES;
 			snd_hdac_bus_update_rirb(bus);
+		}
 		if (!bus->rirb.cmds[addr]) {
 			if (!do_poll)
 				chip->poll_count = 0;
@@ -966,6 +986,8 @@ static int azx_send_cmd(struct hdac_bus *bus, unsigned int val)
 
 	if (chip->disabled)
 		return 0;
+	if (chip->driver_caps & AZX_DCAPS_LS2X_WORKAROUND)
+		udelay(500);
 	if (chip->single_cmd)
 		return azx_single_send_cmd(bus, val);
 	else
@@ -1135,8 +1157,9 @@ static void stream_update(struct hdac_bus *bus, struct hdac_stream *s)
 irqreturn_t azx_interrupt(int irq, void *dev_id)
 {
 	struct azx *chip = dev_id;
+	struct hdac_stream *azx_dev;
 	struct hdac_bus *bus = azx_bus(chip);
-	u32 status;
+	u32 i = 0, status = 0;
 	bool active, handled = false;
 	int repeat = 0; /* count for avoiding endless loop */
 
@@ -1152,8 +1175,19 @@ irqreturn_t azx_interrupt(int irq, void *dev_id)
 		goto unlock;
 
 	do {
-		status = azx_readl(chip, INTSTS);
-		if (status == 0 || status == 0xffffffff)
+		if (chip->driver_caps & AZX_DCAPS_LS2X_WORKAROUND) {
+			list_for_each_entry(azx_dev, &bus->stream_list, list) {
+				status |= (snd_hdac_stream_readb(azx_dev, SD_STS) & SD_INT_MASK) ?
+				    (1 << i) : 0;
+				i++;
+			}
+			status |= (status & ~0) ? (1 << 31) : 0;
+		}
+		else
+			status = azx_readl(chip, INTSTS);
+
+		if (status == 0 ||
+			(status == 0xffffffff && !(chip->driver_caps & AZX_DCAPS_LS2X_WORKAROUND)))
 			break;
 
 		handled = true;
@@ -1170,7 +1204,10 @@ irqreturn_t azx_interrupt(int irq, void *dev_id)
 					udelay(80);
 				snd_hdac_bus_update_rirb(bus);
 			}
-			azx_writeb(chip, RIRBSTS, RIRB_INT_MASK);
+			if (chip->driver_caps & AZX_DCAPS_LS2X_WORKAROUND)
+				azx_writeb(chip, RIRBSTS, status & RIRB_INT_MASK);
+			else
+				azx_writeb(chip, RIRBSTS, RIRB_INT_MASK);
 		}
 	} while (active && ++repeat < 10);
 

AOSC-Tracking/linux

sound/pci/hda/hda_controller.c
@@ -298,6 +298,23 @@ unsigned int azx_get_position(struct azx *chip,
 	int stream = substream->stream;
 	int delay = 0;
 
+	if (chip->driver_caps & AZX_DCAPS_LS2X_WORKAROUND) {
+		pos = chip->get_position[stream](chip, azx_dev);
+
+		if (pos >= azx_dev->fix_prvpos) {
+			pos = pos - azx_dev->fix_prvpos;
+			pos %= azx_dev->core.bufsize;
+		} else {
+			if (azx_dev->fix_prvpos > azx_dev->core.bufsize)
+				pos = (0x100000000ULL + pos-azx_dev->fix_prvpos)
+					% azx_dev->core.bufsize;
+			else
+				pos = pos + azx_dev->core.bufsize - azx_dev->fix_prvpos;
+		}
+
+		return pos;
+	}
+
 	if (chip->get_position[stream])
 		pos = chip->get_position[stream](chip, azx_dev);
 	else /* use the position buffer as default */
@@ -794,8 +811,11 @@ static int azx_rirb_get_response(struct hdac_bus *bus, unsigned int addr,
 
 	for (loopcounter = 0;; loopcounter++) {
 		spin_lock_irq(&bus->reg_lock);
-		if (bus->polling_mode || do_poll)
+		if (bus->polling_mode || do_poll) {
+			if (chip->driver_caps & AZX_DCAPS_LS2X_WORKAROUND)
+				bus->rirb.cmds[addr] %= AZX_MAX_RIRB_ENTRIES;
 			snd_hdac_bus_update_rirb(bus);
+		}
 		if (!bus->rirb.cmds[addr]) {
 			if (!do_poll)
 				bus->poll_count = 0;
@@ -968,6 +988,8 @@ static int azx_send_cmd(struct hdac_bus *bus, unsigned int val)
 
 	if (chip->disabled)
 		return 0;
+	if (chip->driver_caps & AZX_DCAPS_LS2X_WORKAROUND)
+		udelay(500);
 	if (chip->single_cmd)
 		return azx_single_send_cmd(bus, val);
 	else
@@ -1124,8 +1146,9 @@ static void stream_update(struct hdac_bus *bus, struct hdac_stream *s)
 irqreturn_t azx_interrupt(int irq, void *dev_id)
 {
 	struct azx *chip = dev_id;
+	struct hdac_stream *azx_dev;
 	struct hdac_bus *bus = azx_bus(chip);
-	u32 status;
+	u32 i = 0, status = 0;
 	bool active, handled = false;
 	int repeat = 0; /* count for avoiding endless loop */
 
@@ -1141,8 +1164,19 @@ irqreturn_t azx_interrupt(int irq, void *dev_id)
 		goto unlock;
 
 	do {
-		status = azx_readl(chip, INTSTS);
-		if (status == 0 || status == 0xffffffff)
+		if (chip->driver_caps & AZX_DCAPS_LS2X_WORKAROUND) {
+			list_for_each_entry(azx_dev, &bus->stream_list, list) {
+				status |= (snd_hdac_stream_readb(azx_dev, SD_STS) & SD_INT_MASK) ?
+				    (1 << i) : 0;
+				i++;
+			}
+			status |= (status & ~0) ? (1 << 31) : 0;
+		}
+		else
+			status = azx_readl(chip, INTSTS);
+
+		if (status == 0 ||
+			(status == 0xffffffff && !(chip->driver_caps & AZX_DCAPS_LS2X_WORKAROUND)))
 			break;
 
 		handled = true;
@@ -1160,7 +1194,10 @@ irqreturn_t azx_interrupt(int irq, void *dev_id)
 			 * remain unserviced by IRQ, eventually falling back
 			 * to polling mode in azx_rirb_get_response.
 			 */
-			azx_writeb(chip, RIRBSTS, RIRB_INT_MASK);
+			if (chip->driver_caps & AZX_DCAPS_LS2X_WORKAROUND)
+				azx_writeb(chip, RIRBSTS, status & RIRB_INT_MASK);
+			else
+				azx_writeb(chip, RIRBSTS, RIRB_INT_MASK);
 			active = true;
 			if (status & RIRB_INT_RESPONSE) {
 				if (chip->driver_caps & AZX_DCAPS_CTX_WORKAROUND)