2fa60871b22e12c9a6d8589cb2910a86c0e88431
shangyatsen <429839446@qq.com>
2024-03-11 00:14:58 +0800

    LOONGARCH64: BACKPORT: FROMEXT: loongarch debug
    Signed-off-by: shangyatsen <429839446@qq.com>
    Link: https://github.com/FanFansfan/loongson-linux/commit/22c55ab3931c32410a077b3ddb6dca3f28223360
    [Kexy: Resolved minor conflicts in drivers/gpu/drm/xe/xe_bo.c,
     drivers/gpu/drm/xe/xe_guc_ads.c, drivers/gpu/drm/xe/xe_guc_ct.c,
     drivers/gpu/drm/xe/xe_guc_pc.c, drivers/gpu/drm/xe/xe_pt.c,
     drivers/gpu/drm/xe/xe_res_cursor.h, and drivers/gpu/drm/xe/xe_vm.c]
    Signed-off-by: Kexy Biscuit <kexybiscuit@aosc.io>

--- drivers/gpu/drm/xe/xe_guc_ct.c
+++ drivers/gpu/drm/xe/xe_guc_ct.c
@@ -211,7 +211,7 @@ int xe_guc_ct_init(struct xe_guc_ct *ct)
 	struct xe_bo *bo;
 	int err;
 
-	xe_gt_assert(gt, !(guc_ct_size() % PAGE_SIZE));
+	xe_gt_assert(gt, !(guc_ct_size() % XE_PAGE_SIZE));
 
 	ct->g2h_wq = alloc_ordered_workqueue("xe-g2h-wq", WQ_MEM_RECLAIM);
 	if (!ct->g2h_wq)
