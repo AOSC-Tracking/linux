1691f063cf1ddb3c466c58cd9c1ccc33ca845b07
Mingcong Bai <baimingcong@uniontech.com>
2024-07-01 09:13:58 +0800

    [LOONGARCH64] drm/amdgpu: drop cache flush workaround for gfx 7.0 cores
    This commit reverts a2e73f56fa6282481927ec43aa9362c03c2e2104:
    
        drm/amdgpu: Add support for CIK parts
        This patch adds support for CIK parts.  These parts
        are also supported by radeon which is the preferred
        option, so there is a config option to enable support
        for CIK parts in amdgpu for testing.
    
        Acked-by: Christian König <christian.koenig@amd.com>
        Acked-by: Jammy Zhou <Jammy.Zhou@amd.com>
        Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gfx_v7_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v7_0.c
@@ -2117,19 +2117,6 @@ static void gfx_v7_0_ring_emit_fence_gfx(struct amdgpu_ring *ring, u64 addr,
 {
 	bool write64bit = flags & AMDGPU_FENCE_FLAG_64BIT;
 	bool int_sel = flags & AMDGPU_FENCE_FLAG_INT;
-	/* Workaround for cache flush problems. First send a dummy EOP
-	 * event down the pipe with seq one below.
-	 */
-	amdgpu_ring_write(ring, PACKET3(PACKET3_EVENT_WRITE_EOP, 4));
-	amdgpu_ring_write(ring, (EOP_TCL1_ACTION_EN |
-				 EOP_TC_ACTION_EN |
-				 EVENT_TYPE(CACHE_FLUSH_AND_INV_TS_EVENT) |
-				 EVENT_INDEX(5)));
-	amdgpu_ring_write(ring, addr & 0xfffffffc);
-	amdgpu_ring_write(ring, (upper_32_bits(addr) & 0xffff) |
-				DATA_SEL(1) | INT_SEL(0));
-	amdgpu_ring_write(ring, lower_32_bits(seq - 1));
-	amdgpu_ring_write(ring, upper_32_bits(seq - 1));
 
 	/* Then send the real EOP event down the pipe. */
 	amdgpu_ring_write(ring, PACKET3(PACKET3_EVENT_WRITE_EOP, 4));
