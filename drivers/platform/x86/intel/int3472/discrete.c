43fdcbadd8d970cd3f464e70ae46054c3e02fdaa
Kexy Biscuit <kexybiscuit@aosc.io>
2025-02-19 02:48:13 +0800

    AOSCOS: platform/x86: int3472: change polarity to gpio_flags
    Following upstream changes.
    
    Fixes: fc22b06fbd2a ("platform/x86: int3472: Use correct type for "polarity", call it gpio_flags")
    Fixes: "SURFACE: platform/x86: int3472: Remap reset GPIO for INT347E"
    Signed-off-by: Kexy Biscuit <kexybiscuit@aosc.io>

--- drivers/platform/x86/intel/int3472/discrete.c
+++ drivers/platform/x86/intel/int3472/discrete.c
@@ -99,7 +99,7 @@ static int skl_int3472_map_gpio_to_sensor(struct int3472_discrete_device *int347
 	 */
 	if (!strcmp(func, "reset") && !acpi_match_device_ids(int3472->sensor, ov7251_ids)) {
 		func = "enable";
-		polarity ^= GPIO_ACTIVE_LOW;
+		gpio_flags ^= GPIO_ACTIVE_LOW;
 	}
 
 	ret = skl_int3472_fill_gpiod_lookup(&int3472->gpios.table[int3472->n_sensor_gpios],
