423cc944d764aef893e10aaa6ef530721cce6f16
Tianyang Zhang <zhangtianyang@loongson.cn>
2025-05-30 21:45:42 +0800

    LoongArch: Add SCHED_MC (Multi-core scheduler) support
    In order to achieve more reasonable load balancing behavior, add
    SCHED_MC (Multi-core scheduler) support.
    
    The LLC distribution of LoongArch now is consistent with NUMA node,
    the balancing domain of SCHED_MC can effectively reduce the situation
    where processes are awakened to smt_sibling.
    
    Co-developed-by: Hongliang Wang <wanghongliang@loongson.cn>
    Signed-off-by: Hongliang Wang <wanghongliang@loongson.cn>
    Signed-off-by: Tianyang Zhang <zhangtianyang@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- arch/loongarch/include/asm/topology.h
+++ arch/loongarch/include/asm/topology.h
@@ -1,3 +1,11 @@
+/*
+ * @Author: Ilikara 3435193369@qq.com
+ * @Date: 2025-07-07 00:27:03
+ * @LastEditors: Ilikara 3435193369@qq.com
+ * @LastEditTime: 2025-07-07 00:32:30
+ * @FilePath: /linux/arch/loongarch/include/asm/topology.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
  * Copyright (C) 2020-2022 Loongson Technology Corporation Limited
@@ -30,6 +38,14 @@ void numa_set_distance(int from, int to, int distance);
 #endif
 
 #ifdef CONFIG_SMP
+/*
+ * Return cpus that shares the last level cache.
+ */
+static inline const struct cpumask *cpu_coregroup_mask(int cpu)
+{
+	return &cpu_llc_shared_map[cpu];
+}
+
 extern unsigned int __max_packages;
 #define topology_max_packages()                 (__max_packages)
 #define topology_physical_package_id(cpu)	(cpu_data[cpu].package)
